b92bf52d7051f0fb1a89cb0c6b850cf2aa984e2e
Nutchanon28 <65010290@kmitl.ac.th>
2023-04-28 19:36:03 +0700

    <chore> change code for compatibility

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,107 +6,134 @@ import { BiUserCircle } from "react-icons/bi";
 import DataContext from "../context/DataContext";
 
 const Header = () => {
+  const { projectId, setProjectId } = useContext(DataContext);
+  const { userId, setUserId } = useContext(DataContext);
+  const [profileClick, setProfileCLick] = useState(false);
+  const [myProject, setMyProject] = useState([]);
+  const [newProjectId, setNewProjectId] = useState(1);
 
-    const { projectId, setProjectId } = useContext(DataContext)
-    const { userId, setUserId } = useContext(DataContext)
-    const [profileClick, setProfileCLick] = useState(false)
-    const [myProject, setMyProject] = useState([])
-    const [newProjectId, setNewProjectId] = useState(1)
+  async function getMyProject() {
+    const response = await fetch(
+      `http://127.0.0.1:8000/view_my_project/${userId}`
+    );
+    const responseJson = await response.json();
+    setMyProject(responseJson);
+  }
 
-    async function getMyProject() {
-        const response = await fetch(`http://127.0.0.1:8000/view_my_project/${userId}`)
-        const responseJson = await response.json()
-        setMyProject(responseJson)
-    }
+  function toggleProfileClick() {
+    setProfileCLick(!profileClick);
+  }
 
-    function toggleProfileClick() {
-        setProfileCLick(!profileClick)
-    }
+  async function onStartProjectCLick() {
+    const newProject = {
+      project_name: `Project${Date.now().toString()}`,
+      category: "Art",
+      project_image: "image",
+      project_duration: 1,
+      project_detail: "",
+      pledge_goal: 1,
+      pledge_reward: [],
+      creator_id: userId,
+      payment_detail: {
+        legal_first_name: "",
+        legal_last_name: "",
+        email_address: "",
+        date_of_birth: "",
+        home_address: "",
+        city: "",
+        state: "",
+        postal_code: "",
+        phone_number: "",
+        account_number: "",
+        bank: "",
+      },
+    };
+    await fetch("http://127.0.0.1:8000/add_project", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newProject),
+    });
 
-    async function onStartProjectCLick() {
-        const newProject = { 
-            project_name: `Project${Date.now().toString()}`, 
-            category: "Art", 
-            project_image: "image", 
-            project_duration: 1, 
-            project_detail: "",
-            pledge_goal: 1,
-            pledge_reward: [], 
-            creator_id: userId,
-            payment_detail: {
-                legal_first_name: "",
-                legal_last_name: "", 
-                email_address: "",
-                date_of_birth: "", 
-                home_address: "", 
-                city: "", 
-                state: "", 
-                postal_code: "", 
-                phone_number: "", 
-                account_number: "", 
-                bank: ""
-            }
-        }
-        await fetch("http://127.0.0.1:8000/add_project", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(newProject)
-          })
+    const response = await fetch(`http://127.0.0.1:8000/get_last_project`);
+    const responseJson = await response.json();
+    setNewProjectId(responseJson.id);
+    console.log(responseJson);
+    setProjectId(responseJson.id);
+    console.log(`Your project id is ${responseJson.id}`);
+  }
 
-        const response = await fetch(`http://127.0.0.1:8000/get_last_project`)
-        const responseJson = await response.json()
-        setNewProjectId(responseJson.id)
-        console.log(responseJson)
-        setProjectId(responseJson.id)
-        console.log(`Your project id is ${responseJson.id}`)
-    }
+  useEffect(() => {
+    getMyProject();
+    console.log(myProject);
+  }, [profileClick]);
 
-    useEffect(() => {
-        getMyProject()
-        console.log(myProject)
-    }, [profileClick])
+  let myProjectElements = null;
+  if (myProject.length) {
+    myProjectElements = myProject.map((project) => {
+      return (
+        <p>
+          <Link to={`start-project/${project.id}`}>
+            {project._Project__project_name}
+          </Link>
+        </p>
+      );
+    });
+  }
 
-    let myProjectElements = null
-    if(myProject.length) {
-        myProjectElements = myProject.map((project) => {
-            return (
-            <p><Link to={`start-project/${project.id}`}>{project._Project__project_name}</Link></p> 
-            )
-        })
-    }
-
-    return (
-        <div className="header">
-            <div className='container'>
-                <div className='header-con'>
-                    <ul className='left-menu'>
-                        <li><a href="#">Discover</a></li>
-                        <li onClick={onStartProjectCLick}><Link to={"start-project"}>Start a project</Link></li>
-                    </ul>
-                    <div className='logo'>
-                    <Link to=""><h2>KICKSTARTER</h2></Link>
-                    </div>
-                    <ul className='right-menu'>
-                        <li><a href="#">Search <HiSearch className='search-icon'/></a></li>
-                        <li><a href="#"><BiUserCircle className='user-icon' onClick={toggleProfileClick}/></a></li>
-                    </ul>
-                </div>
-                
-            </div>
-            <div className={`menu-tab + ${profileClick ? "" : "hidden"}`}>
-                <div className='section-menu-tab'>
-                    <p>YOUR ACCOUNT</p>
-                    <p>Profile</p>
-                    <p>Settings</p>
-                </div>
-                <div className="section-menu-tab">
-                    <p>CREATED PROJECTS</p>
-                    {myProjectElements}
-                    <Link to="created-project"><p>view all</p></Link>
-                </div>
-            </div>
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="header-con">
+          <ul className="left-menu">
+            <li>
+              <a href="#">Discover</a>
+            </li>
+            <li onClick={onStartProjectCLick}>
+              <Link to={"start-project"}>Start a project</Link>
+            </li>
+          </ul>
+          <div className="logo">
+            <Link to="">
+              <h2>KICKSTARTER</h2>
+            </Link>
+          </div>
+          <ul className="right-menu">
+            <li>
+              <a href="#">
+                Search <HiSearch className="search-icon" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <BiUserCircle
+                  className="user-icon"
+                  onClick={toggleProfileClick}
+                />
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+      </div>
+      <div className={`menu-tab + ${profileClick ? "" : "hidden"}`}>
+        <div className="section-menu-tab">
+          <p>YOUR ACCOUNT</p>
+          <p>
+            <Link to="profile">Profile</Link>
+          </p>
+          <p>
+            <Link to="setting">Settings</Link>
+          </p>
+        </div>
+        <div className="section-menu-tab">
+          <p>CREATED PROJECTS</p>
+          {myProjectElements}
+          <Link to="created-project">
+            <p>view all</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
