88d4d766c7c3b18d67a8b8e7cc3f0816b3d7b515
janipang <pang.janista@gmail.com>
2023-05-02 00:48:49 +0700

    feat: input button in  add-update

--- src/component/OtherPage/AddUpdate.js
+++ src/component/OtherPage/AddUpdate.js
@@ -3,6 +3,10 @@ import { useState, useEffect } from "react";
 import "../../css/OtherPage/AddUpdate.css";
 
 function AddUpdate() {
+  
+  const [title ,setTitle] = useState("")
+  const [body ,setBody] = useState("")
+  const [defaultLayout, setDefaultLayout] = useState(true);
 
     return (
       <div className="addUpdate">
@@ -16,38 +20,56 @@ function AddUpdate() {
                 <p>New Update</p>
                 <p>choose an update style</p>
                 <div className="template-option">
-                  <div className="temp-op">
+                  <div className="temp-op" onClick={() => setDefaultLayout(true)}>
                     <p>Freeform</p>
                     <p>Your story, your way</p>
                     </div>
-                  <div className="temp-op">
+                  <div className="temp-op" onClick={() => setDefaultLayout(false)}>
                     <p>Q+A</p>
                     <p>Follow Our lead</p>
                   </div>
                 </div>
                 <p className="text">Build trust with your backers.Learn about project updates and why they're important.Never include your backer's names, addresses, and other personal details in your updates.</p>
               </div>
-              <div className="detail">
-                <p>Title</p>
-                <div className="box1"></div>
-                <p>Body</p>
-                <div className="box2"></div>
+              <div className="freeform">
+                {defaultLayout && (
+                  <>
+                  <form className="detail" action="/action_page.php">
+                    <p>Title</p>
+                    <input className="box1" type="text" id = "title" value = {title} placeholder="Title" onChange={(e) => setTitle(e.target.value)}/>
+                    <p>Body</p>
+                    <input className="box2" type="text" id = "body" value = {body} placeholder="Detail" onChange={(e) => setBody(e.target.value)}/>
+                    <div className="footer">
+                      <div className="button">Delete draft</div>
+                      <div className="button">Publish</div> 
+                      {/* if u click "Publish" it will call app.post add_update function from api*/}
+                    </div>
+                  </form>
+                  </>
+                )}
+              </div>
+              <div className="QnA">
+                {!defaultLayout && (
+                  <>
+                  <form className="detail" action="/action_page.php">
+                    <p>Question</p>
+                    <input className="box1" type="text" id = "title" value = {title} placeholder="Title" onChange={(e) => setTitle(e.target.value)}/>
+                    <p>Answer</p>
+                    <input className="box2" type="text" id = "body" value = {body} placeholder="Detail" onChange={(e) => setBody(e.target.value)}/>
+                    <div className="footer">
+                      <div className="button">Delete draft</div>
+                      <div className="button">Publish</div> 
+                      {/* if u click "Publish" it will call app.post add_update function from api*/}
+                    </div>
+                  </form>
+                  </>
+                )}
               </div>
             </div>
             <div className="setting">
-              <p>Visible to</p>
-              <div className="privacy-choice"> </div>
-              <p>Notifications</p>
-              <div className="privacy-choice"> </div>
+              
             </div>
 
-          </div>
-          <div className="footer">
-            <div className="button">Delete draft</div>
-            <div className="button">Preview</div>
-            <div className="button">Draft saved</div>
-            <div className="button">Publish</div> 
-            {/* if u click "Publish" it will call app.post add_update function from api*/}
           </div>
         </div>
       </div>
